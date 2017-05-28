0f2dc8b9d45d1ef132e4c27a80ef9155de49ef14
Asami Doi <doiasami1219@gmail.com>
2017-05-28 18:42:15 +0900

    add: anagram searcher

---
+++ anagram_searcher.py
@@ -0,0 +1,12 @@
+class AnagramSearcher:
+
+    def __init__(self):
+        pass
+
+    def search(self, word):
+        pass
+
+
+if __name__ == '__main__':
+    anagram_searcher = AnagramSearcher()
+    anagram_searcher.search("star")
