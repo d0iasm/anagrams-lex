71334ce5d7880585387237a720e7ee614162bf91
Asami Doi <doiasami1219@gmail.com>
2017-05-28 19:15:57 +0900

    modify: get string of a specific line

--- anagram_searcher.py
+++ anagram_searcher.py
@@ -1,10 +1,24 @@
+import linecache
+
+
 class AnagramSearcher:
 
     def __init__(self):
-        pass
+        self.dict = "./dict/anagram_words"
+        self.num = sum(1 for line in open(self.dict))
 
     def search(self, word):
-        pass
+        low = 0
+        high = self.num
+
+        word = word.lower().strip()
+        char_list = list(word)
+        char_list.sort()
+        #sign = "".join(char_list)
+        #print(char_list)
+
+        center_words = linecache.getline(self.dict, (low+high) / 2)
+        print(center_words.split())
 
 
 if __name__ == '__main__':
