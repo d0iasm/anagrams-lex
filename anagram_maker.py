d4768f45cedeacd76c86e5ced6ae74716d27f0b6
Asami Doi <doiasami1219@gmail.com>
2017-05-28 13:36:13 +0900

    modfiy: sort a word

--- anagram_maker.py
+++ anagram_maker.py
@@ -6,14 +6,25 @@ class AnagramMaker:
     def __init__(self):
         pass
 
-    def read(self, name):
-        f = open(name)
+    def sign(self, file_name):
+        
+        def list_to_dict(key_list, val_list):
+            return dict(zip(key_list, val_list))
+
+        f = open(file_name)
         line = f.readline()
         while line:
-            print(line)
+            lower_line = line.lower().strip()
+            char_list = list(lower_line)
+            char_list.sort()
+            print(char_list)
             line = f.readline()
+
+        key_list = ["a", "b", "c"]
+        val_list = ["hoge", "hoge", "hoge"]
+        print(list_to_dict(key_list, val_list))
 
 
 if __name__ == '__main__':
     anagram_maker = AnagramMaker()
-    anagram_maker.read("./dict/original_words_short")
+    anagram_maker.sign("./dict/original_words_short")
