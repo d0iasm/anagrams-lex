0fef9f5ece08a6efc7aad345ecffc379b2aa3e2a
Asami Doi <doiasami1219@gmail.com>
2017-05-30 09:18:43 +0900

    modify: subset true with input word and dict sign

--- anagram_searcher.py
+++ anagram_searcher.py
@@ -44,16 +44,19 @@ class AnagramSearcher:
             center_words = linecache.getline(self.dict, center)
             center_words_list = center_words.split()
             center_sign = center_words_list[0]
+            center_sign_list = list(center_sign)
+            
+            if all(x in char_list for x in center_sign_list):
+                print(self.high_score(center_words_list))
+                return self.search(input('--> '))
+
             if center_sign < sign:
                 low = center + 1
             elif center_sign > sign:
                 high = center - 1
-            else:
-                print(self.high_score(center_words_list))
-                return self.search(input('--> '))
         
         print("not find anagrams")
-        self.search(input('--> '))
+        return self.search(input('--> '))
 
 
 if __name__ == '__main__':
